a0ba5138cacfe735ec6612cde39ce37a2a20d542
Henrique Velloso <henriquevelloso@truetapp.com>
2018-12-13 16:24:26 -0200

    FIRST

---
+++ Broker MQTT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Broker MQTT
+//
+//  Created by Henrique Velloso on 13/12/18.
+//  Copyright © 2018 Henrique Velloso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '9.0'
+
+target 'Broker MQTT' do
+  use_frameworks!
+
+  	pod 'MQTTClient'			#https://github.com/novastone-media/MQTT-Client-Framework
+	pod 'Hue'				    #https://github.com/hyperoslo/Hue
+
+end
+ 
